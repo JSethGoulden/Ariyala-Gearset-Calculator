58d3581771d95091a1ed26e610f32a19392ee8f5
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2020-01-27 13:00:52 -0600

    add copy to clipboard feature

--- Firefox/popup.js
+++ Firefox/popup.js
@@ -1,3 +1,4 @@
+let gearsetData = {}
 function updatePopupHtml(data) {
     document.getElementById("raidPieces").append(document.createTextNode(data.raidPieces))
     document.getElementById("tomePieces").append(document.createTextNode(data.tomePieces))
@@ -6,8 +7,24 @@ function updatePopupHtml(data) {
     document.getElementById("food").append(document.createTextNode(data.food))
     document.getElementById("foodUrl").href = "http://garlandtools.org/db/#item/" + data.foodId
 }
+
+function copyToClipboard() {
+  document.getElementById("clipboardIcon").className = "glyphicon glyphicon-ok"
+  document.getElementById("clipboardIcon").style.color = "#6F0"
+
+  let copyString = ""
+  copyString += "Raid - " + document.getElementById("raidPieces").innerText + " \n"
+  copyString += "Tome - " + document.getElementById("tomePieces").innerText + " \n"
+  copyString += "Upgrade Materials - " + document.getElementById("upgradePieces").innerText + " \n"
+  copyString += "Total Tomestones - " + document.getElementById("totalTomes").innerText + " \n"
+  copyString += "Food - " + document.getElementById("food").innerText
+
+  navigator.clipboard.writeText(copyString)
+}
+
 browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
     browser.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-      updatePopupHtml(response.data);
+      document.getElementById("clipboard").addEventListener("click", copyToClipboard)
+      updatePopupHtml(response.data)
     });
 });
